f99c304992d9f5e521937b429c18e52e99f13a30
archana <archana@ubuntu-amd64.myguest.virtualbox.org>
2024-04-20 19:01:15 +0530

    Assignment 1

---
+++ finder-app/writer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+writefile=$1
+writestr=$2
+
+if [ ! "$#" -eq  2  ]
+then
+	exit 1
+elif [  "$#" -eq 1 ]
+then
+	echo "Missing one of arguments"
+fi
+writefile1="${writefile%/*}"
+#echo $writefile1
+mkdir -p $writefile1
+echo "$writestr" > "$writefile"
+
+if [ ! "$?" ]
+then 
+	echo "File cannot be created"
+	exit 1
+fi
+
+
